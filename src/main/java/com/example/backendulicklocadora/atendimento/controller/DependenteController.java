0c00dbea78091cb57eec148e26e4327da7ebb2a2
Gabriel Ulick <gabrieludb@hotmail.com>
2024-10-30 14:55:45 -0300

    fix: services dependente e socio com @Autowired nos devidos controllers

--- src/main/java/com/example/backendulicklocadora/atendimento/controller/DependenteController.java
+++ src/main/java/com/example/backendulicklocadora/atendimento/controller/DependenteController.java
@@ -2,6 +2,7 @@ package com.example.backendulicklocadora.atendimento.controller;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,16 +18,18 @@ import com.example.backendulicklocadora.exception.GenericOperation;
 @RequestMapping("dependente")
 public class DependenteController extends GenericController<Dependente> {
 
-    private DependenteService dependenteService;
+    private final DependenteService dependenteService;
 
+    @Autowired
     public DependenteController(DependenteService dependenteService) {
         super(dependenteService);
+        this.dependenteService = dependenteService;
     }
     
     @DeleteMapping("/ativo/{id}")
     @GenericOperation(description = "Exclusão lógica de um registro")
     public void exclusaoLogica(@PathVariable UUID id) {
-        dependenteService.excluir(id);
+        System.out.println("id dele" + id);
+        dependenteService.exclusaoLogica(id);
     }
-
 }
--- src/main/java/com/example/backendulicklocadora/atendimento/controller/SocioController.java
+++ src/main/java/com/example/backendulicklocadora/atendimento/controller/SocioController.java
@@ -2,6 +2,7 @@ package com.example.backendulicklocadora.atendimento.controller;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,16 +18,18 @@ import com.example.backendulicklocadora.exception.GenericOperation;
 @RequestMapping("socio")
 public class SocioController extends GenericController<Socio> {
 
-    private SocioService socioService;
+    private final SocioService socioService;
 
+    @Autowired
     public SocioController(SocioService socioService) {
         super(socioService);
+        this.socioService = socioService;
     }
 
     @DeleteMapping("/ativo/{id}")
     @GenericOperation(description = "Exclusão lógica de um registro")
     public void exclusaoLogica(@PathVariable UUID id) {
-        socioService.excluir(id);
+        socioService.exclusaoLogica(id);
     }
 
 }
